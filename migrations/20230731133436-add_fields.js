875e1ee312244e45167695daf2fa543196080638
Je4stine <je4stine@gmail.com>
2023-07-31 17:22:50 +0300

    Changes to migration

--- migrations/20230731133436-add_fields.js
+++ migrations/20230731133436-add_fields.js
@@ -3,13 +3,13 @@
 /** @type {import('sequelize-cli').Migration} */
 module.exports = {
   async up (queryInterface, Sequelize) {
-    return queryInterface.addColumn('Users', 'newField', {
+    return queryInterface.addColumn('Users', 'originalName', {
       type: Sequelize.STRING,
       allowNull: true,
     });
   },
 
   async down (queryInterface, Sequelize) {
-    return queryInterface.removeColumn('Users', 'newField');
+    return queryInterface.removeColumn('Users', 'originalName');
   }
 };
